593cc5e03d5986c5e497425c5623f03297135dcd
Mayur Sharma <mayur@mayursharma.in>
2021-04-24 22:38:46 +0530

    Improved : Find GCD or HCF of two numbers

--- src/main/java/com/home/learning/mathematics/FindGCDOrHCF.java
+++ src/main/java/com/home/learning/mathematics/FindGCDOrHCF.java
@@ -4,12 +4,21 @@ public class FindGCDOrHCF {
     
     public static void main(String[] args) {
         FindGCDOrHCF problem = new FindGCDOrHCF();
-        Integer number1 = 10;
-        Integer number2 = 150;
+        Integer number1 = 12;
+        Integer number2 = 15;
 
         Integer gcd = problem.findGCDOrHCF(number1, number2);
         System.out.println("GCD or HCF of " + number1 + " and " + number2 +
         " is " + gcd);
+        gcd = problem.improvedFindGCDOrHCF(number1, number2);
+        System.out.println("Improved : GCD or HCF of " + number1 + " and " + number2 +
+        " is " + gcd);
+    }
+    private Integer improvedFindGCDOrHCF(Integer number1, Integer number2) {
+        if(number2 == 0)
+            return number1;
+        else 
+            return findGCDOrHCF(number2, number1%number2);
     }
 
     private Integer findGCDOrHCF(Integer number1, Integer number2) {
